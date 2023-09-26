d9b4e3919b0b5009465af4d954c57b71e59c9e48
Arthurgameplays1 <145415733+Arthurgameplays1@users.noreply.github.com>
2023-09-25 22:54:09 -0300

    Update index.js

--- index.js
+++ index.js
@@ -10,9 +10,8 @@ let appleX = 5;
 let appleY = 5;
 let tailLength = 2;
 const snakeParts = [];
-const speed = 7; // Define a velocidade aqui
-let score = 0; // Inicialize a pontuação
-let isGameRunning = true;
+const speed = 7; // Define the speed here
+let score = 0; // Initialize the score
 
 function clearScreen() {
   ctx.fillStyle = 'black';
@@ -20,42 +19,13 @@ function clearScreen() {
 }
 
 function drawGame() {
-  if (isGameRunning) {
-    changeSnakePosition();
-    clearScreen();
-    drawSnake();
-    checkCollision();
-    drawApple();
-    drawScore();
-    setTimeout(drawGame, 1000 / speed);
-  }
-}
-
-function isGameOver() {
-  let gameOver = false;
-
-  if (headX < 0 || headX >= tileCount || headY < 0 || headY >= tileCount) {
-    gameOver = true; // Game over se a cobra sair dos limites do campo
-  }
-
-  for (let i = 0; i < snakeParts.length; i++) {
-    let part = snakeParts[i];
-    if (part.x === headX && part.y === headY) {
-      gameOver = true; // Game over se a cobra colidir com ela mesma
-      break;
-    }
-  }
-
-  if (gameOver) {
-    isGameRunning = false; // Pausa o jogo
-    ctx.fillStyle = 'orange'; // Defina a cor laranja
-    ctx.fillRect(0, 0, canvas.width, canvas.height); // Preencha a tela com a cor laranja
-    ctx.fillStyle = 'white';
-    ctx.font = '50px verdana';
-    ctx.fillText('Game Over!', canvas.clientWidth / 6.5, canvas.clientHeight / 2);
-  }
-
-  return gameOver;
+  changeSnakePosition();
+  clearScreen();
+  drawSnake();
+  checkCollision();
+  drawApple();
+  drawScore();
+  setTimeout(drawGame, 1000 / speed);
 }
 
 function checkCollision() {
@@ -65,13 +35,32 @@ function checkCollision() {
     tailLength++;
     score++;
   }
+
+  if (headX < 0 || headY < 0 || headX >= tileCount || headY >= tileCount) {
+    // Game over logic
+    location.reload(); // Reload the page when game over occurs
+    return;
+  }
+
+  for (let i = 0; i < snakeParts.length; i++) {
+    const part = snakeParts[i];
+    if (part.x === headX && part.y === headY) {
+      // Game over logic
+      location.reload(); // Reload the page when game over occurs
+      return;
+    }
+  }
 }
 
 function drawSnake() {
+  ctx.fillStyle = 'green';
   for (let i = 0; i < snakeParts.length; i++) {
     const part = snakeParts[i];
-    ctx.fillStyle = 'green'; // Use a cor verde para os quadrados da cobra
     ctx.fillRect(part.x * tileCount, part.y * tileCount, tileSize, tileSize);
+  }
+  snakeParts.push(new snakePart(headX, headY));
+  while (snakeParts.length > tailLength) {
+    snakeParts.shift(); // Remove the tail if it's too long
   }
 }
 
@@ -81,73 +70,52 @@ function drawApple() {
 }
 
 function changeSnakePosition() {
-  const newHeadX = (headX + xVelocity + tileCount) % tileCount;
-  const newHeadY = (headY + yVelocity + tileCount) % tileCount;
+  const newHeadX = headX + xVelocity;
+  const newHeadY = headY + yVelocity;
+
+  // Check for collision with walls (game over condition)
+  if (newHeadX < 0 || newHeadX >= tileCount || newHeadY < 0 || newHeadY >= tileCount) {
+    // Game over logic
+    location.reload(); // Reload the page when game over occurs
+    return;
+  }
 
   headX = newHeadX;
   headY = newHeadY;
-
-  snakeParts.unshift({ x: headX, y: headY });
-
-  if (snakeParts.length > tailLength) {
-    snakeParts.pop();
-  }
 }
 
 document.body.addEventListener('keydown', keyDown);
 
 function keyDown(event) {
-  if (isGameRunning) {
-    switch (event.keyCode) {
-      case 38: // Seta para cima
-        if (yVelocity !== 1) {
-          yVelocity = -1;
-          xVelocity = 0;
-        }
-        break;
-      case 40: // Seta para baixo
-        if (yVelocity !== -1) {
-          yVelocity = 1;
-          xVelocity = 0;
-        }
-        break;
-      case 37: // Seta para a esquerda
-        if (xVelocity !== 1) {
-          yVelocity = 0;
-          xVelocity = -1;
-        }
-        break;
-      case 39: // Seta para a direita
-        if (xVelocity !== -1) {
-          yVelocity = 0;
-          xVelocity = 1;
-        }
-        break;
-    }
-  } else {
-    // Se o jogo estiver parado e a tecla Enter for pressionada, reinicie o jogo
-    if (event.keyCode === 13) {
-      restartGame();
-    }
+  switch (event.keyCode) {
+    case 38: // Up arrow
+      if (yVelocity !== 1) {
+        yVelocity = -1;
+        xVelocity = 0;
+      }
+      break;
+    case 40: // Down arrow
+      if (yVelocity !== -1) {
+        yVelocity = 1;
+        xVelocity = 0;
+      }
+      break;
+    case 37: // Left arrow
+      if (xVelocity !== 1) {
+        yVelocity = 0;
+        xVelocity = -1;
+      }
+      break;
+    case 39: // Right arrow
+      if (xVelocity !== -1) {
+        yVelocity = 0;
+        xVelocity = 1;
+      }
+      break;
   }
 }
 
-function restartGame() {
-  isGameRunning = true;
-  headX = 10;
-  headY = 10;
-  xVelocity = 0;
-  yVelocity = 0;
-  appleX = 5;
-  appleY = 5;
-  tailLength = 2;
-  snakeParts.length = 0;
-  score = 0;
-  clearScreen();
-  drawGame();
-}
-
-// Classe para partes da cobra
+// Class for snake parts
 class snakePart {
   constructor(x, y) {
     this.x = x;
@@ -160,8 +128,5 @@ function drawScore() {
   ctx.font = '10px Verdana';
   ctx.fillText('Score: ' + score, canvas.clientWidth - 50, 10);
 }
-
-// Iniciar o jogo com uma direção inicial para a cobra
-xVelocity = 1; // Começar indo para a direita
 
 drawGame();
